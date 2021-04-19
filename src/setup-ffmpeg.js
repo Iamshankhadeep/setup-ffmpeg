4aa7a38c0c1ca65018fd27a9cf6aa7f7f36ce356
Shankhadeep Dey <shankhadeepdey99@gmail.com>
2021-04-19 23:11:20 +0530

    update ffmpeg find version

--- src/setup-ffmpeg.js
+++ src/setup-ffmpeg.js
@@ -17,7 +17,7 @@ export async function find(os, arch, options = {}) {
   const octokit = new Octokit({ auth: options.token });
   const response = await octokit.repos.listReleases({ owner, repo });
   const release = response.data.find(({ tag_name }) =>
-    tag_name.startsWith("ffmpeg-")
+    tag_name.startsWith("ffmpeg-4.1.4")
   );
   return {
     release,
